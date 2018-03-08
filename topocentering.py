eb0ed42d458a94a0ed65b6ab2d98c459f8d947d8
sam-fahey <37160531+sam-fahey@users.noreply.github.com>
2018-03-08 13:50:57 -0600

    Update topocentering.py
    Swapping out patchwork old script with functional definitions.

--- topocentering.py
+++ topocentering.py
@@ -1,66 +1,65 @@
 #!/usr/bin/env python
 
-import ephem
+import ephem, math, sys
 import numpy as np
-import math
+from datetime import datetime
 
-import icecube
-from icecube import grbllh, umdtools
-from icecube.umdtools import cache
-import bg_src_North, bg_src_South
-from bg_src_North import Bg_srcs as srcs_north
-from bg_src_South import Bg_srcs as srcs_south
-
-srcs_north = srcs_north()
-srcs_south = srcs_south()
-
-dec = np.append(srcs_north.zenith, srcs_south.zenith)-np.pi/2
-ra = np.append(srcs_north.azimuth, srcs_south.azimuth)
-t_obs = np.append(srcs_north.t, srcs_south.t)
-
-print "\n Begin barycentering... \n \n"
-
-arecibo_lat = np.radians(18. + 20./60 + 39./3600) # North: 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13
-arecibo_lon = -1*np.radians(66. + 45./60 + 10./3600)
-arecibo_D_to_IC = 1.622 # in Earth radii
-gbt_lat = np.radians(38. + 25./60 + 59./3600) # North: 0, 14, 15, 16, 17, 18
-gbt_lon = -1*np.radians(79. + 50./60 + 23./3600)
-gbt_D_to_IC = 1.801
-parkes_lat = -1*np.radians(33.) # North: 1, 2, 19
-parkes_lon = np.radians(148 + 15./60 + 44./3600) # South: ALL (0 - 8)
-parkes_D_to_IC = 0.954
-
-icecube = ephem.Observer()
-icecube.elevation = 1000
-icecube.lon, icecube.lat = 0., np.radians(-90.)
-
-w = True
-if w == True: 
-    writefile = open('/home/sfahey/public_html/FRB/barycentric_corrections.txt', 'w')
-    writefile.write('===================\n|  Northern FRBs  |\n===================\n')
-
-for i in range(29):
+def observer( longitude, latitude, elevation ):
     obs = ephem.Observer()
-    obs.elevation = 1000
-    if i in np.arange(3,14): obs.lon, obs.lat, site, D = arecibo_lon, arecibo_lat, 'Arecibo', arecibo_D_to_IC
-    elif i in [0,14,15,16,17,18,19]: obs.lon, obs.lat, site, D = gbt_lon, gbt_lat, 'Green Bank', gbt_D_to_IC
-    elif i in [1,2]: obs.lon, obs.lat, site, D = parkes_lon, parkes_lat, 'Parkes', parkes_D_to_IC
-    elif i > 19: obs.lon, obs.lat, site, D = parkes_lon, parkes_lat, 'Parkes', parkes_D_to_IC
-    obs.date, icecube.date = ephem.Date(t_obs[i]), ephem.Date(t_obs[i])
+    obs.lon = longitude
+    obs.lat = latitude
+    obs.elevation = elevation
+    return obs
+
+def sources( ra, dec, time ):
+    if not hasattr(ra, '__len__'): ra = [ra]
+    if not hasattr(dec, '__len__'): dec = [dec]
+    if type(time) == type(datetime(2010,1,1,0,0,0)): time = [time]
+    else:
+        print "Error: sources() input 'time' must be a datetime object."
+        sys.exit()
+    if not len(ra) == len(dec) and len(ra) == len(time):
+        print "Error: numbers of source parameters are inconsistent!"
+        sys.exit()
+
+    src = {}
+    src['ra'] = np.array(ra, dtype=float)
+    src['dec'] = np.array(dec, dtype=float)
+    src['t'] = np.array(time, dtype=datetime)
+    return src
+
+def topo_correction( detector_obs, followup_obs, sources ):
+
+    time_diffs = np.zeros(len(sources['ra']))
+    for j in range(len(sources['ra'])):
+        ra = sources['ra'][j]
+        dec = sources['dec'][j]
+        t = sources['t'][j]
+
+        detector_obs.date = ephem.Date(t)
+        followup_obs.date = ephem.Date(t)
+
+        detector_separation = ephem.separation( detector_obs.radec_of(0., np.pi/2), [ra, dec] )
+        followup_separation = ephem.separation( followup_obs.radec_of(0., np.pi/2), [ra, dec] )
+
+        detector_core_transit = (detector_obs.elevation + 6.317e6) * math.cos( detector_separation )
+        followup_core_transit = (followup_obs.elevation + 6.317e6) * math.cos( followup_separation )
+
+        time_diff = ( detector_core_transit - followup_core_transit ) / 3.e8
+        time_diffs[j] = time_diff
+
+    return time_diffs
+
+def run_example():
+
+    icecube = observer( np.radians(0.), np.radians(-90.), 2835 )
+    arecibo = observer( np.radians(66. + 45./60 + 10./3600)*-1., np.radians(18. + 20./60 + 39./3600), 497 )
+    frb121102 = sources( (5. + 32./60)/24 * 2*np.pi, np.radians(33. + 8./60), datetime(2012, 11, 02, 06, 47, 17) )
+
+    print topo_correction( arecibo, icecube, frb121102 )
+    # output: [0.03177779]
+    # signal expected to traverse IceCube (followup) 31.78 ms after Arecibo (detector) detection of source.
+
+    return None
+
     
-    ice_ra, ice_dec = icecube.radec_of(0., np.pi/2) # RA and Dec of IceCube's zenith at time t
-    obs_ra, obs_dec = obs.radec_of(0., np.pi/2) # RA and Dec of observatory's zenith at time t
-
-    ice_dot_FRB = ephem.separation([float(ephem.degrees(ice_ra)), float(ephem.degrees(ice_dec))],[ra[i], dec[i]])
-    obs_dot_FRB = ephem.separation([float(ephem.degrees(obs_ra)), float(ephem.degrees(obs_dec))],[ra[i], dec[i]])
-
-    s = math.cos(float(ice_dot_FRB)) - math.cos(float(obs_dot_FRB))
-    r_Earth = 6.371e6 # Earth mean radius in meters
-    light_time = (s*r_Earth/3.e8)*1.e3 # Difference in ms
-    if w == True:
-        if i == 20: writefile.write('\n===================\n|  Southern FRBs  |\n===================\n')
-        writefile.write("\nFRB_Dec = %f\nSite = %s\nObs. Time = %s\nsep(Obs,FRB) = %s\nsep(Ice,FRB) = %s\nBarycentric Corr. = %f ms\n"%(dec[i]*180./np.pi, site, obs.date, obs_dot_FRB, ice_dot_FRB, -1*light_time))
-
-if w == True:
-    writefile.write("\nNOTE: Observation time + Barycentric Correction = Time at IceCube via plane-wave propagation")
-    writefile.close()
